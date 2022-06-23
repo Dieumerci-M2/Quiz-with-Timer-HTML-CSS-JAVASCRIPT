f5bb648c0e2e584871660951dcfe16b677d9f9e4
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-06-23 15:59:56 +0200

    feat : improve Javascript-page

--- app.js
+++ app.js
@@ -1,3 +1,152 @@
+// SELECTION DES ELEMENTS 
+
 const mainlog = document.querySelector('.header.log')
 const maingame = document.querySelector('.header.game')
 const mainoutput = document.querySelector('.header.output')
+
+// SELECTIONS DES ELEMENTS DE LA SECTION LOG
+
+let warningMessage = document.querySelector('.formlog .warning')
+const buttonlog = document.querySelector('.btn.log #valider')
+const nom = document.querySelector('.formlog #nom')
+const email = document.querySelector('.formlog #email')
+
+// SELECTION DES ELEMENTS DE LA SECTION GAME
+
+const question = document.querySelector('.container h6')
+const mbrQuestion = document.querySelector('.mbrquestion')
+const mbrQuestionLeft = document.querySelector('.mbrquestion div #leftNumber')
+const mbrQuestionRight = document.querySelector('.mbrquestion div #rightNumber')
+const mbrQuestionTimer = document.querySelector('.mbrquestion div #timer')
+const niveauExt = document.querySelector('.niveauExt')
+let niveauFull = document.querySelector('.niveaufull')
+let listQuestion = document.querySelector('.text div')
+let questionOne = document.querySelector('questionOne')
+let questionTwo = document.querySelector('questionTwo')
+let questionTree = document.querySelector('questionTree')
+let questionFour = document.querySelector('questionFour')
+const btnGameLeft = document.querySelector('.btngameleft')
+const btnGameRight = document.querySelector('.btn.foot .btngameright')
+
+// SELECTION DES ELEMENTS DE LA SECTION OUTPUT
+
+const resultNom = document.querySelector('.EnteteOutput h1')
+const resultEmail = document.querySelector('.ShowEmailOutput h3')
+let showcheck = document.querySelector('.ShowCheckPositive')
+let showresult = document.querySelector('.ShowResult span')
+const returnBack = document.querySelector('.btn.returnBack')
+
+// LES ECOUTEURS 
+
+mainlog.addEventListener('click', ()=>{
+
+})
+
+maingame.addEventListener('click' , ()=>{
+
+})
+
+mainoutput.addEventListener('click', ()=>{
+     
+})
+
+// LES ECOUTEURS POUR LA SECTION LOG
+
+warningMessage.addEventListener('click' , ()=>{
+    
+})
+ buttonlog.addEventListener('click', ()=>{
+    
+ })
+
+ nom.addEventListener('click' , ()=>{
+    
+ })
+
+ email.addEventListener('click', ()=>{
+    
+ })
+
+ // LES ECOUTEURS POUR LA SECTION GAME
+
+ question.addEventListener('click', ()=>{
+   
+ })
+
+ mbrQuestion.addEventListener('click', ()=>{
+     
+ })
+
+ mbrQuestionLeft.addEventListener('click',()=>{
+    
+ })
+
+ mbrQuestionRight.addEventListener('click',()=>{
+    
+ })
+
+ mbrQuestionTimer.addEventListener('click', ()=>{
+    
+ })
+
+ niveauExt.addEventListener('click', ()=>{
+    
+ })
+
+ niveauFull.addEventListener('click', ()=>{
+     
+ })
+
+ listQuestion.addEventListener('click', ()=>{
+    
+ })
+ questionOne.addEventListener('click',()=>{
+
+ })
+
+ questionTwo.addEventListener('click', ()=>{
+    niveauFull.style.display = 'block';
+ })
+
+ questionTree.addEventListener('click', ()=>{
+
+ })
+
+ questionFour.addEventListener('click', ()=>{
+
+ })
+
+ btnGameLeft.addEventListener('click', ()=>{
+
+ })
+
+ btnGameRight.addEventListener('click', ()=>{
+    e.style.background = 'black';
+ })
+
+ // LES ECOUTEURS POUR LA SECTION OUTPUT
+
+ resultNom.addEventListener('click', ()=>{
+
+ })
+
+ resultEmail.addEventListener('click', ()=>{
+
+ })
+
+ showcheck.addEventListener('click', ()=>{
+
+ })
+
+ showresult.addEventListener('click', ()=>{
+
+ })
+
+ returnBack.addEventListener('click', ()=>{
+
+ })
+
+
+
+
+
