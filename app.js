863e390101a9bd35fd7fc5b8303c9c0b5855e0b6
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-06-28 13:51:43 +0200

    refactor : improve jvascript code

--- app.js
+++ app.js
@@ -21,42 +21,44 @@ const mbrQuestionRight = document.querySelector('.mbrquestion div #rightNumber')
 const mbrQuestionTimer = document.querySelector('.mbrquestion div #timer')
 const niveauExt = document.querySelector('.niveauExt')
 let niveauFull = document.querySelector('.niveaufull')
-let listQuestion = document.querySelector('.text div input')
+let listQuestion = document.querySelectorAll('.text input')
 const btnGameLeft = document.querySelector('.btngameleft')
 const btnGameRight = document.querySelector('.btn.foot .btngameright')
 const labelOfQuestion = document.querySelectorAll('.text span')
 let stock = -1;
+let score = 0;
 
 // SELECTION DES ELEMENTS DE LA SECTION OUTPUT
 
 const resultNom = document.querySelector('.EnteteOutput h1')
 const resultEmail = document.querySelector('.ShowEmailOutput h3')
-let showcheck = document.querySelector('.ShowCheckPositive')
+let showcheck = document.querySelector('.ShowCheckPositive #result_symbole')
 let showresult = document.querySelector('.ShowResult #resultNumber')
 const returnBack = document.querySelector('.btn.returnBack #accueil')
 
 // CONSTRUCTEUR POUR LES QUESTIONNAIRES
 
-function Question(question="", answer=[""]) {
+function Question(question="", answer=[""], correct =0) {
    this.question = question;
    this.answer = answer;
+   this.correct = correct;
 }
 const questionList = [
-   new Question(`Quel est le type d'un fichier javascript?`, [`.ts`,`.jsx`,`.js`,`.jar`]),
-   new Question(`Quel est le type d'un fichier CSS?`, [`.jsx`,`.js`,`.css`,`html`]),
-   new Question(`Quel est le type d'un fichier HTML?`,[`.jsx`,`.js`,`.css`,`.html`]),
-   new Question(`Parmis ces langages, lequel est utiliser à la fois en Front et en Backend?`, [`HTML`,`CSS`,`JAVASCRIPT`,`PHP`]),
-   new Question(`Quel sustème d'exploitation est plus utiliser dans le monde?`,[`Ubuntu`,`MacOs`,`Debian`,`Windows10`]),
-   new Question(`Quel Framework JavaScript est plus utiliser en 2022?`, [`Angular`,`Vue`,`React`, `Ember`]),
-   new Question(`Parmi ces Frameworks, lequel a été conçus par l'équipe Google?`, [`Angular`,`Vue`,`React`,`Ember`]),
-   new Question(`Parmi ces Frameworks, lequel a été conçus par l'équipe Facebook?`, [`Angular`,`React`,`Vue`,`Ember`]),
-   new Question(`Parmi ces Frameworks, lequel a été conçus par Evan You?`, [`Angular`,`React`,`Vue`,`Ember`]),
-   new Question(`Quel est la ville célèbre en présence des grandes enseignes telle que : Google, Apple, facebook etc.`, [`Washingston DC`,`Toronto`,`siliscon Valley`,`Massachusetts`]),
-   new Question(`Quel est le nom du PDG de Facebook?`, [`Bill Gate`,`Mark Zuckerberg`,`Elon Musk`,`Andy Jassy`]),
-   new Question (`Quel est le langage de programmation le plus utiliser en Machine Learning?`, [`JavaScript`,`Java`,`C++`,`Python`]),
+   new Question(`Quel est le type d'un fichier javascript?`, [`.ts`,`.jsx`,`.js`,`.jar`],2),
+   new Question(`Quel est le type d'un fichier CSS?`, [`.jsx`,`.js`,`.css`,`html`],2),
+   new Question(`Quel est le type d'un fichier HTML?`,[`.jsx`,`.js`,`.css`,`.html`],3),
+   new Question(`Parmis ces langages, lequel est utiliser à la fois en Front et en Backend?`, [`HTML`,`CSS`,`JAVASCRIPT`,`PHP`],2),
+   new Question(`Quel sustème d'exploitation est plus utiliser dans le monde?`,[`Ubuntu`,`MacOs`,`Debian`,`Windows10`],3),
+   new Question(`Quel Framework JavaScript est plus utiliser en 2022?`, [`Angular`,`Vue`,`React`, `Ember`],2),
+   new Question(`Parmi ces Frameworks, lequel a été conçus par l'équipe Google?`, [`Angular`,`Vue`,`React`,`Ember`],0),
+   new Question(`Parmi ces Frameworks, lequel a été conçus par l'équipe Facebook?`, [`Angular`,`React`,`Vue`,`Ember`],1),
+   new Question(`Parmi ces Frameworks, lequel a été conçus par Evan You?`, [`Angular`,`React`,`Vue`,`Ember`],2),
+   new Question(`Quel est la ville célèbre en présence des grandes enseignes telle que : Google, Apple, facebook etc.`, [`Washingston DC`,`Toronto`,`siliscon Valley`,`Massachusetts`],2),
+   new Question(`Quel est le nom du PDG de Facebook?`, [`Bill Gate`,`Mark Zuckerberg`,`Elon Musk`,`Andy Jassy`],1),
+   new Question (`Quel est le langage de programmation le plus utiliser en Machine Learning?`, [`JavaScript`,`Java`,`C++`,`Python`],3),
    new Question(`Pourquoi on met le script en bas dans la balyse body?`, [`pour qu il soit lu en dernier`,`pour le design`,`pour le style`,`pour la forme`]),
    new Question(`Parmi ces Frameworks, lequel est utiliser en Python web?`, [`Ember`,`Angular`,`Django`,`React`]),
-   new Question(`Pour n'est pas avoir 0/15 dans ce Quiz, parmi ces langages de programmation, lequel est votre préféré?`, [`Ruby on Rails`,`Python`,`JavaScript`,`C & C++`])
+   new Question(`Pour n'est pas avoir 0/15 dans ce Quiz, parmi ces langages de programmation, lequel est votre préféré?`, [`Ruby on Rails`,`Python`,`JavaScript`,`C & C++`],1)
 ]
 
 // LES FONCTIONS UTILISERS
@@ -116,21 +118,35 @@ const nextQuestion = ()=>{
    if(stock == 15){
       mainoutput.style.display = 'block';
       maingame.style.display = 'none';
-   }else if(stock <= 7){
-      showresult.innerHTML = `<span class="mdi mdi-close-circle-outline" id="result_symbole"></span>` 
+      
    }
    mbrQuestionLeft.textContent = stock+1;
-   showresult.textContent = stock;
+   showresult.textContent = score;
    question.textContent = questionList[stock].question;
    labelOfQuestion.forEach((element, index)=>{
       element.textContent = questionList[stock].answer[index];
    })
+   listQuestion.forEach((element, index)=>{
+      if(element.checked && questionList[stock-1].correct == index ){
+         score++; 
 
+      }
+      element.checked = false;
+   })
+
+}
+
+const showCheckingSuccess = ()=>{
+   if(score <= 7){
+      showcheck.innerHTML = `<span class="mdi mdi-close-circle-outline" ></span>` ;
+}else{
+   showcheck.innerHTML = `<span class=class="mdi mdi-check-circle-outline" ></span>` ;
+}
 }
 
 const dropAll = ()=>{
    mainoutput.style.display = 'block';
-   maingame.style.display = 'none';
+   maingame.style.display = `none`;
 }
 
 
@@ -145,7 +161,10 @@ function conteur() {
       niveauFull.style.width = `${timerWidth}%`;
       startTime--;
    }else if(startTime==0){
-      question.textContent = questionList[stock].question;
+      // question.textContent = questionList[stock].question;
+      nextQuestion();
+      startTime = 59;
+      timerWidth = 100;
    }
 }
 // LA FONCTION DE RETOUR A LA PAGE D'ACCUEIL
@@ -174,22 +193,22 @@ email.addEventListener('input', ()=>{
 
  // LES ECOUTEURS POUR LA SECTION GAME
 
- listQuestion.addEventListener('click', ()=>{
-    
- })
-
  btnGameLeft.addEventListener('click', ()=>{
    dropAll(this);
  })
 
  btnGameRight.addEventListener('click', ()=>{
     nextQuestion();
+    startTime = 59;
+    timerWidth = 100;
  })
 
  // LES ECOUTEURS POUR LA SECTION OUTPUT
 
  returnBack.addEventListener('click', ()=>{
-   returnHome(this)
+   returnHome(this);
+   showCheckingSuccess(this);
+   location.reload();
  })
 
 
